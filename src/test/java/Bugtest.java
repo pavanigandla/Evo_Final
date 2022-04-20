1e9619a0119617b582e36cf64d3661fa4c83e920
pavanigandla <90361165+pavanigandla@users.noreply.github.com>
2022-04-19 23:41:50 -0500

    Add files via upload

---
+++ src/test/java/Bugtest.java
@@ -0,0 +1,27 @@
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class Bugtest {
+
+    public static boolean debug = false;
+
+    @Test
+    public void test1() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "Bugtest.test1");
+        AVLTree aVLTree0 = new AVLTree();
+        AVLTree.Node node2 = aVLTree0.find((int) '4');
+        AVLTree.Node node3 = aVLTree0.getRoot();
+        aVLTree0.delete(100);
+        aVLTree0.insert((int) (byte) 10);
+        AVLTree.Node node8 = aVLTree0.getRoot();
+        aVLTree0.insert((int) (short) 0);
+        // during test generation this statement threw an exception of type java.lang.NullPointerException in error
+        aVLTree0.delete(10);
+    }
+}
+
